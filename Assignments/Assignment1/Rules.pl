d25a66a572dcd76f6ece8a56c442187fe328beea
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-03-03 15:30:12 +0300

    [Fixing bugs and Enhancements] Fix problems in backtracking and add multiple path searching

--- Assignments/Assignment1/Rules.pl
+++ Assignments/Assignment1/Rules.pl
@@ -18,8 +18,11 @@ param(5,mapSize).
 :- dynamic ballPos/2.
 :- dynamic visited/2.
 :- dynamic path/2.
+:- dynamic optimalPathCount/1.
 
 ballPos(0,0).
+
+
 
 resetMap :-
     retractall(mapBoarders(_,_)),
@@ -58,7 +61,9 @@ initMap :-
     s(Xs,Ys),
     asserta(mapStart(Xs,Ys)),
     changeBallPos(Xs,Ys),
-    asserta(visited(Xs,Ys)).
+    asserta(visited(Xs,Ys)),
+    PathLimitCount is Z*Z,
+    setOptimalPathCount(PathLimitCount).
 
 
 restartMap :-
@@ -84,38 +89,57 @@ reached(X,Y) :-
     write(Xv), write(Yv),
     write("\n----------Reached!!!----------\n").
 
-search(X,Y) :-
-    % search(X+1,Y).
-    asserta(visited(X,Y)),
-    reached(X,Y);
-    move(X,Y),
-    pass(X,Y).
 
+% This only will make it search once
+% backtrackSearch(X,Y) :-
+%     asserta(visited(X,Y)),
+%     reached(X,Y);
+%     move(X,Y),
+%     pass(X,Y).
+
+
+% Save the path
+great(X,Y,N) :-
+    setOptimalPathCount(N),
+    write("\n----------Reached!!!----------\n").
+
+
+% This to limit the backtrack to the best result we have till now or not
+checkOptimalPath(N) :-
+    optimalPathCount(Best),
+    not(N > Best).
+
+backtrackSearch(X,Y, N) :-
+    asserta(visited(X,Y)),
+    checkOptimalPath(N),
+    reached(X,Y) -> great(X,Y,N); not(reached(X,Y)) ->  move(X,Y, N),
+    pass(X,Y).
 
 
 pass(X,Y) :-
     true.
 
-
-move(X,Y) :-
-    format('\n#Current ~d ~d', [X,Y]),
+move(X,Y, N) :-
+    format('\n ----Step---- ~d', [N]),
+    Nnew is N +1,
+    format('\n#Current in ~d ~d and deciding', [X,Y]),
     nextUp(X,Y,Xu,Yu),
-    format('\nUp ~d ~d', [Xu, Yu]),
-    search(Xu,Yu),
+    format('\nUp to ~d ~d', [Xu, Yu]),
+    backtrackSearch(Xu,Yu,Nnew),
     retract(visited(Xu,Yu));
     nextDown(X,Y,Xd,Yd),
-    format('\nDown ~d ~d', [Xd, Yd]),
-    search(Xd,Yd),
+    format('\nDown to ~d ~d', [Xd, Yd]),
+    backtrackSearch(Xd,Yd,Nnew),
     retract(visited(Xd,Yd));
     nextRight(X,Y,Xr,Yr),
-    format('\nRight ~d ~d', [Xr, Yr]),
-    search(Xr,Yr),
+    format('\nRight to ~d ~d', [Xr, Yr]),
+    backtrackSearch(Xr,Yr,Nnew),
     retract(visited(Xr,Yr));
     nextLeft(X,Y,Xl,Yl),
-    format('\nLeft ~d ~d', [Xl, Yl]),
-    search(Xl,Yl),
+    format('\nLeft to ~d ~d', [Xl, Yl]),
+    backtrackSearch(Xl,Yl,Nnew),
     retract(visited(Xl,Yl)).
-    
+
 
 nextRight(X,Y, Xr, Yr) :-
     Yr is Y,
@@ -145,12 +169,16 @@ changeBallPos(X,Y) :-
     retractall(ballPos(_,_)),
     assert(ballPos(X,Y)).
 
+setOptimalPathCount(N) :-
+    retractall(optimalPathCount(_)),
+    assert(optimalPathCount(N)).
+
 
 main :-
-    consult("input.txt"),
+    consult("input.pl"),
     s(Xs,Ys),
     initMap ,
-    search(Xs,Ys).
+    backtrackSearch(Xs,Ys,0).
 
 % empty (X,Y):-
 %     \+ o(X,Y),
